fc3e81a59f422f34bd5450b35f3e5b2fc93fc157
Christoph Herrmann <developer@dragonprojects.de>
2016-11-22 20:08:57 +0100

    node-red-contrib-assets-imdb-rating

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Dependency Status](https://david-dm.org/dragonprojects/node-red-contrib-assets-imdb-rating.svg)](https://david-dm.org/dragonprojects/node-red-contrib-assets-imdb-rating)
+[![devDependency Status](https://david-dm.org/dragonprojects/node-red-contrib-assets-imdb-rating/dev-status.svg)](https://david-dm.org/dragonprojects/node-red-contrib-assets-imdb-rating?type=dev)
---
+++ src/assets-imdb-rating.js
@@ -0,0 +1,28 @@
+const imdb = require('imdb-api');
+
+module.exports = (RED) => {
+  RED.nodes.registerType('assets-imdb-rating', function NODE(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    node.on('input', async (msg) => {
+      if (!msg.payload.title) {
+        node.warn('no title in the payload');
+        return;
+      }
+      const asset = msg.payload;
+      asset.imdb = await new Promise((resolve) => {
+        const originalTitle = asset.title
+          .replace(' (Hot from the US)', '')
+          .replace(/ \(Season .*\)/, '');
+        imdb.get(originalTitle, (err, data) => {
+          if (err) {
+            resolve({ rating: 'N/A' });
+          } else {
+            resolve({ rating: data.rating });
+          }
+        });
+      });
+      node.send(msg);
+    });
+  });
+};
