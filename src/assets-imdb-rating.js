25680881d3fae5a4f4cd8000419759ee646c9176
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-12-13 15:15:39 +0100

    Use node.status

--- src/assets-imdb-rating.js
+++ src/assets-imdb-rating.js
@@ -4,36 +4,45 @@ module.exports = (RED) => {
   RED.nodes.registerType('assets-imdb-rating', function NODE(config) {
     RED.nodes.createNode(this, config);
     const node = this;
+    node.status({});
 
     node.on('input', async (msg) => {
+      node.status({ fill: 'grey', shape: 'dot', text: `requesting...` });
       const assets = msg.payload;
 
       if (!Array.isArray(assets)) {
-        node.warn('payload is not an assets array');
+        node.status({ fill: 'yellow', shape: 'dot', text: 'payload not an assets array' });
+        node.warn('payload not an assets array');
         return;
       }
 
-      await Promise.all(
-        assets
-          .filter(asset => asset.title)
-          .map(asset => new Promise((resolve) => {
-            const originalTitle = asset.title
-              .replace(' (Hot from the US)', '')
-              .replace(/ \(Season .*\)/, '');
-            imdb.get(originalTitle, (err, data) => {
-              if (err) {
-                asset.imdb = { rating: 'N/A' };
-              } else {
-                asset.imdb = { rating: data.rating };
-              }
-              if (asset.imdb.rating !== 'N/A') {
-                asset.title += ` (${asset.imdb.rating})`;
-              }
-              resolve();
-            });
-          })
-        )
-      );
+      try {
+        await Promise.all(
+          assets
+            .filter(asset => asset.title)
+            .map(asset => new Promise((resolve) => {
+                const originalTitle = asset.title
+                  .replace(' (Hot from the US)', '')
+                  .replace(/ \(Season .*\)/, '');
+                imdb.get(originalTitle, (err, data) => {
+                  if (err) {
+                    asset.imdb = { rating: 'N/A' };
+                  } else {
+                    asset.imdb = { rating: data.rating };
+                  }
+                  if (asset.imdb.rating !== 'N/A') {
+                    asset.title += ` (${asset.imdb.rating})`;
+                  }
+                  resolve();
+                });
+              })
+            )
+        );
+        node.status({});
+      } catch (e) {
+        node.status({ fill: 'yellow', shape: 'dot', text: 'requesting error' });
+        node.warn(`requesting error (${e.message})`);
+      }
 
       node.send(msg);
     });
