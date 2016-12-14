18f51ccc509e124b188ff33cb31ec9356c438041
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-12-14 15:54:38 +0100

    node-red-contrib-dnode

--- src/assets-imdb-rating.js
+++ src/assets-imdb-rating.js
@@ -1,49 +1,32 @@
+const DNode = require('node-red-contrib-dnode');
 const imdb = require('imdb-api');
 
-module.exports = (RED) => {
-  RED.nodes.registerType('assets-imdb-rating', function NODE(config) {
-    RED.nodes.createNode(this, config);
-    const node = this;
-    node.status({});
-
-    node.on('input', async (msg) => {
-      node.status({ fill: 'grey', shape: 'dot', text: 'requesting...' });
-      const assets = msg.payload;
-
-      if (!Array.isArray(assets)) {
-        node.status({ fill: 'yellow', shape: 'dot', text: 'payload not an assets array' });
-        node.warn('payload not an assets array');
-        return;
-      }
-
-      try {
-        await Promise.all(
-          assets
-            .filter(asset => asset.title)
-            .map(asset => new Promise((resolve) => {
-              const originalTitle = asset.title
-                .replace(' (Hot from the US)', '')
-                .replace(/ \(Season .*\)/, '');
-              imdb.get(originalTitle, (err, data) => {
-                if (err) {
-                  asset.imdb = { rating: 'N/A' };
-                } else {
-                  asset.imdb = { rating: data.rating };
-                }
-                if (asset.imdb.rating !== 'N/A') {
-                  asset.title += ` (${asset.imdb.rating})`;
-                }
-                resolve();
-              });
-            })),
-        );
-        node.status({});
-      } catch (e) {
-        node.status({ fill: 'yellow', shape: 'dot', text: 'requesting error' });
-        node.warn(`requesting error (${e.message})`);
-      }
-
-      node.send(msg);
-    });
+module.exports = DNode.createNode('assets-imdb-rating', (dnode) => {
+  dnode.onInput(async (msg) => {
+    const assets = msg.payload;
+    if (!Array.isArray(assets)) {
+      throw new Error('missing assets array in payload');
+    }
+    await Promise.all(
+      assets
+        .filter(asset => asset.title)
+        .map(asset => new Promise((resolve) => {
+          const originalTitle = asset.title
+            .replace(' (Hot from the US)', '')
+            .replace(/ \(Season .*\)/, '');
+          imdb.get(originalTitle, (err, data) => {
+            if (err) {
+              asset.imdb = { rating: 'N/A' };
+            } else {
+              asset.imdb = { rating: data.rating };
+            }
+            if (asset.imdb.rating !== 'N/A') {
+              asset.title += ` (${asset.imdb.rating})`;
+            }
+            resolve();
+          });
+        })),
+    );
+    dnode.sendMessage(msg);
   });
-};
+});
